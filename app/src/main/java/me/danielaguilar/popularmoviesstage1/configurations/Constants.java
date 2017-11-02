c02aa0d2360d8c18e71249ca0c7e127c8168adf4
Daniel Aguilar <danielaguilar@MacBook-Pro-de-Daniel.local>
2017-11-01 21:34:00 -0300

    remove api key.

--- app/src/main/java/me/danielaguilar/popularmoviesstage1/configurations/Constants.java
+++ app/src/main/java/me/danielaguilar/popularmoviesstage1/configurations/Constants.java
@@ -5,7 +5,7 @@ package me.danielaguilar.popularmoviesstage1.configurations;
  */
 
 public class Constants {
-    public static final String API_KEY              = "34738023d27013e6d1b995443764da44";
+    public static final String API_KEY              = "";//TODO add api_key here
     public static final String API_BASE_URL         = "https://api.themoviedb.org";
     public static final String IMAGES_BASE_URL      = "http://image.tmdb.org/t/p/w342";//"http://image.tmdb.org/t/p/w185";
     public static final String API_BASE_PATH        = "/3/movie/";
