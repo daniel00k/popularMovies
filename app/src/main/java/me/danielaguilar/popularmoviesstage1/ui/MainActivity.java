ce03a89e48557871b421663748d070997dc11900
Daniel Aguilar <danielaguilar@MacBook-Pro-de-Daniel.local>
2017-11-01 23:05:03 -0300

    Add scrollbar and connectivity check

--- app/src/main/java/me/danielaguilar/popularmoviesstage1/ui/MainActivity.java
+++ app/src/main/java/me/danielaguilar/popularmoviesstage1/ui/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import org.json.JSONException;
 
--- app/src/main/java/me/danielaguilar/popularmoviesstage1/utils/NetworkUtils.java
+++ app/src/main/java/me/danielaguilar/popularmoviesstage1/utils/NetworkUtils.java
@@ -1,5 +1,8 @@
 package me.danielaguilar.popularmoviesstage1.utils;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 
 import java.io.IOException;
@@ -61,6 +64,16 @@ public class NetworkUtils {
         } finally {
             urlConnection.disconnect();
         }
+    }
+
+    public static boolean isConectionAvailable(Context context){
+        ConnectivityManager cm =
+                (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+
     }
 
 }
